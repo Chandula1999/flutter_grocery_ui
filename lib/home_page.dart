46bd4771fed72878e3e7d9c539844cf617e8e20b
Chandula Samarasinghe <Chandulas1999@gmail.com>
2024-08-10 22:01:23 +0530

    updated mian.dart

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_grocery_ui/widgets/app_bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: appBarLeading,
+        title: appBarTitle,
+        actions: appBarActions,
+      ),
+    );
+  }
+}
